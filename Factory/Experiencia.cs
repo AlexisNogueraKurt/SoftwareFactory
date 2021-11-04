005b8e772d90181fe3ea8cf16ffe733b1ef592d2
Alexis Noguera <87494818+AlexisNogueraKurt@users.noreply.github.com>
2021-11-04 16:04:26 -0300

    Create Experiencia.cs

---
+++ Factory/Experiencia.cs
@@ -0,0 +1,9 @@
+namespace Factory
+{
+    public class Experiencia
+    {
+        public int cuil { get; set; }
+        public int id { get; set; }
+        public int calificacion { get; set; } 
+    }
+}
